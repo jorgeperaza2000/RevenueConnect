a9ef62df007b9e368928f1ed75e7e4afae475eea
Jorge Peraza <jorgeperaza2000@gmail.com>
2021-08-25 19:55:59 +0200

    Modificando numero de telefono

--- legal_action_army/app/public/js/app.js
+++ legal_action_army/app/public/js/app.js
@@ -1,124 +1,64 @@
-$("#question-two-for-me, #question-two-for-loved").hide();
-$("#question-three-for-me, #question-three-for-loved").hide();
-$("#question-four-for-me, #question-four-for-loved").hide();
-$("#question-five-for-me, #question-five-for-loved").hide();
-
-$(".form").attr('disabled', true);
-$("#steps").show();
-
-var current_cuestion = parseInt($("#current-question").html());
-
 $(document).ready(function(){
-    /* START FOR ME */
-    $(".step-two-for-me").click(function(){
-        $('input[name="question-one"]').val($(".step-two-for-me").text());
-        $("#question-one").fadeOut('fast');
-        $("#question-two-for-me").fadeIn('slow');
-        $("#current-question").html(++current_cuestion);
+    $("#current-question").html("1");
+    var current_question = 1;
+    $.getJSON( "js/questions.json", function( data ) {
+        var questions = data.paraquat.questions;
+        $("#total-question").html(questions.length);
+
+        var container = $("question-example");
+        questions.forEach((element, index) => {
+            $("#questions").append(
+                '<div class="container-fluid" id="question-'+ index +'">\
+                    <div class="row justify-content-center">\
+                        <div class="col-md-6 col-sm-12 text-center">\
+                            <span class="yellow" id="question">'+element.question+'</span>\
+                        </div>\
+                    </div>\
+                </div>');
+            if ( element.answers_type == 'yes_no' ) {
+                $("#question-" + index).append(
+                    '<div class="row justify-content-center pb-5">\
+                        <div class="col-md-2 p-2">\
+                            <button type="button" class="btn btn-primary w-100 step-two-for-me">'+element.answers_value[0]+'</button>\
+                        </div>\
+                        <div class="col-md-2 p-2">\
+                            <button type="button" class="btn btn-success w-100 step-two-for-loved">'+element.answers_value[1]+'</button>\
+                        </div>\
+                    </div>'
+                    );
+            } else if ( element.answers_type == 'input' ) {
+                $("#question-" + index).append(
+                    '<div class="row justify-content-center pb-5">\
+                        <div class="col-md-1 p-2">\
+                            <input type="text" name="'+element.input_name+'" class="form-control text-center" placeholder="'+element.input_placeholcer+'" maxlength="4">\
+                        </div>\
+                        <div class="col-md-1 p-2">\
+                            <button type="button" class="btn btn-success w-100 step-four-for-me">'+element.btn_text+'</button>\
+                        </div>\
+                    </div>'
+                );
+            } else if ( element.answers_type == 'select' ) {
+                var select_option = "";
+                element.answers_value.forEach((answer, i) => {
+                    select_option += '<option value="'+answer+'">'+answer+'</option>';
+                });
+                console.log(select_option);
+                $("#question-" + index).append(
+                    '<div class="row justify-content-center pb-5">\
+                        <div class="col-md-2 col-sm-10 text-center pb-2">\
+                            <select class="form-select form-control form-select-md" name="'+element.select_name+'">\
+                                <option selected value="">-- Select --</option>\
+                                ' + select_option + '\
+                            </select>\
+                        </div>\
+                        <div class="col-md-1 p-2">\
+                            <button type="button" class="btn btn-success w-100 step-four-for-me">'+element.btn_text+'</button>\
+                        </div>\
+                    </div>'
+                );
+            }
+            
+        });
+
     });
-
-    $(".step-three-for-me").click(function(){
-        $('input[name="question-two"]').val($(this).html());
-        $("#question-two-for-me").fadeOut('fast');
-        $("#question-three-for-me").fadeIn('slow');
-        $("#current-question").html(++current_cuestion);
-    });
-
-    $('input[name="step-three-for-me"]').keyup(function() {
-        if ( $(this).val().length == "4" ) {
-            $('.step-four-for-me').attr('disabled', false);
-        } else {
-            $('.step-four-for-me').attr('disabled', true);
-        }
-    });
-
-    $(".step-four-for-me").click(function(){
-        if ( $('input[name="step-three-for-me"]').val().length == "4" ) {
-            $('input[name="question-three"]').val($('input[name="step-three-for-me"]').val());
-            $("#question-three-for-me").fadeOut('fast');
-            $("#question-four-for-me").fadeIn('slow');
-            $("#current-question").html(++current_cuestion);
-        } else {
-            $('.step-four').attr('disabled', true);
-        }
-    });
-
-    $(".step-five-for-me").click(function(){
-        $('input[name="question-four"]').val($('select[name="step-four-for-me"] option:selected').val());
-        $("#question-four-for-me").fadeOut('fast');
-        $("#question-five-for-me").fadeIn('slow');
-        $("#current-question").html(++current_cuestion);
-    });
-
-    $(".step-six-for-me").click(function(){
-        $('input[name="question-five"]').val($(".step-six-for-me").html());
-        $("#question-five-for-me").fadeOut('slow');
-        $(".form").attr('disabled', false);
-        $("#steps").fadeOut('slow');
-    });
-    /* END FOR ME */
-
-    /* START FOR A LOVED ONE */
-    $(".step-two-for-loved").click(function(){
-        $('input[name="question-one"]').val($(".step-two-for-loved").text());
-        $("#question-one").fadeOut('fast');
-        $("#question-two-for-loved").fadeIn('slow');
-        $("#current-question").html(++current_cuestion);
-    });
-
-    $(".step-three-for-loved").click(function(){
-        $('input[name="question-two"]').val($(this).html());
-        $("#question-two-for-loved").fadeOut('fast');
-        $("#question-three-for-loved").fadeIn('slow');
-        $("#current-question").html(++current_cuestion);
-    });
-
-    $('input[name="step-three-for-loved"]').keyup(function() {
-        if ( $(this).val().length == "4" ) {
-            $('.step-four-for-loved').attr('disabled', false);
-        } else {
-            $('.step-four-for-loved').attr('disabled', true);
-        }
-    });
-
-    $(".step-four-for-loved").click(function(){
-        if ( $('input[name="step-three-for-loved"]').val().length == "4" ) {
-            $('input[name="question-three"]').val($('input[name="step-three-for-loved"]').val());
-            $("#question-three-for-loved").fadeOut('fast');
-            $("#question-four-for-loved").fadeIn('slow');
-            $("#current-question").html(++current_cuestion);
-        } else {
-            $('.step-four').attr('disabled', true);
-        }
-    });
-
-    $(".step-five-for-loved").click(function(){
-        $('input[name="question-four"]').val($('select[name="step-four-for-loved"] option:selected').val());
-        $("#question-four-for-loved").fadeOut('fast');
-        $("#question-five-for-loved").fadeIn('slow');
-        $("#current-question").html(++current_cuestion);
-    });
-
-    $(".step-six-for-loved").click(function(){
-        $('input[name="question-five"]').val($(".step-six-for-loved").html());
-        $("#question-five-for-loved").fadeOut('fast');
-        $(".form").attr('disabled', false);
-        $("#steps").fadeOut('slow');
-    });
-    /* END FOR A LOVED ONE */
-
-   /*  $('.step-three').on('change', function() {
-        $("#question-three").show();
-        $('input[name="question-two"]').val(this.value);
-    });*/
-
-    /* $(".step-four-no").click(function(){
-        $("#question-three").show();
-        $('input[name="question-three"]').val($(".step-four-no").text());
-    });
-
-    $(".step-four-yes").click(function(){
-        $("#question-three").show();
-        $('input[name="question-three"]').val($(".step-four-yes").text());
-    }); */
 });
